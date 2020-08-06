39cc21cfa70d01cd5963047a1a57fdadc1e0ab84
OnyxDevs13 <corvin.prath@gmail.com>
2020-08-06 15:52:35 +0200

    Cat command - Can now send images from r/cats

--- src/commands/cat.js
+++ src/commands/cat.js
@@ -5,24 +5,55 @@ const Discord = require('discord.js');
 const CAT_API_KEY = "77754d84-55e9-48bd-a31e-853ba386ec6e";
 const CAT_API_URL = 'https://api.thecatapi.com/';
 
+const feedLimit = 100;
+
 module.exports = {
     name: "cat",
     description: "Sends a random cat image! Mreeeow!",
-    async execute(message) {
-        const images = await loadImage();
-        const image = images[0];
-        const breed = image.breeds[0];
+    async execute(message, args) {
+        if (!args.length) {
+            sendAPIImage();
+        }
+        else if("reddit".startsWith(args[0].toLowerCase())) {
+            const feed = await loadRedditFeed("cats", "top", feedLimit);
+            let post = feed.data.children[getRandomInt(0, 100)];
+            while (post.data.is_self || post.data.is_video || post.data.is_gallery) {
+                post = feed.data.children[getRandomInt(0, 100)];
+            }
+            let url = post.data.url;
+            if (url.includes("imgur")) {
+                url = url + ".png";
+            }
+            const file = new Discord.MessageAttachment(url);
+            const catEmbed = {
+                title: post.data.title,
+                color: "#FF4500",
+                image: {
+                    url: file.attachment
+                },
+            };
 
-        const file = new Discord.MessageAttachment(image.url);
+            message.channel.send({ embed: catEmbed });
+            return;
+        }
 
-        const catEmbed = {
-            title: `Here's a ${breed.name}!`,
-            image: {
-                url: file.attachment
-            },
-        };
+        async function sendAPIImage() {
+            const images = await loadImage();
+            const image = images[0];
+            const breed = image.breeds[0];
 
-        message.channel.send({ embed: catEmbed });
+            const file = new Discord.MessageAttachment(image.url);
+
+            const catEmbed = {
+                title: `Here's a ${breed.name}!`,
+                color:"#3282b8",
+                image: {
+                    url: file.attachment
+                },
+            };
+
+            message.channel.send({ embed: catEmbed });
+        }
     }
 };
 
@@ -46,4 +77,20 @@ async function loadImage() {
     catch (imgError) {
         console.error(imgError);
     }
+}
+
+async function loadRedditFeed(subreddit, sort, limit) {
+    const subUrl = `https://www.reddit.com/r/${subreddit}/${sort}`;
+    try {
+        return await fetch(`${subUrl}.json?raw_json=1&limit=${limit}`).then(response => response.json());
+    }
+    catch (imgError) {
+        console.error(imgError);
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
