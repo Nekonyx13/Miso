d8703897cf67e6e3091d2f388e3e269adf81a8cf
OnyxDevs13 <corvin.prath@gmail.com>
2020-08-27 23:08:51 +0200

    added youtbe-search module

--- src/utils/music.js
+++ src/utils/music.js
@@ -1,55 +1,65 @@
 const ytdl = require('ytdl-core-discord');
 const { getYouTubeSearchResults } = require("../apis/youtubeData");
+const { YOUTUBE_API_KEY } = require("../../secrets.json");
 const ytlist = require('youtube-playlist');
+const ytsearch = require('youtube-search');
 
 exports.resolveSong = async (arg) => {
-    let url;
-    if(validateYouTubeUrl(arg)) {
-        url = arg;
+    let url = arg;
+    const host = resolveHost(arg);
+    switch(host) {
+    case 'search': {
+        const search = await ytsearch(arg, { 
+            maxResults: 1,
+            order: 'relevance',
+            type: 'video',
+            key: YOUTUBE_API_KEY,
+        });
+        url = search.results[0].link;
+        break;
     }
-    else {
-        const search = await getYouTubeSearchResults(arg, 1);
-        const id = search.items[0].id.videoId;
-        url = `https://www.youtube.com/watch?v=${id}`;
+    case 'spotify': {
+        // DUMMY
     }
-    const songInfo = await ytdl.getInfo(url);
+    }
+    const songInfo = await ytdl.getBasicInfo(url);
     return {
         title: songInfo.title,
         url: songInfo.video_url,
-        thumbnail: songInfo.player_response.videoDetails.thumbnail.thumbnails[2].url,
+        thumbnail: songInfo.player_response.videoDetails.thumbnail.thumbnails[0].url,
     };
 };
 
-exports.resolvePlaylist = async (url) => {
-    const videoUrls = await ytlist(url, 'url').then(p => p.data.playlist);
-    const playlist = [];
-    for await (const videoUrl of videoUrls) {
-        const songInfo = await ytdl.getInfo(videoUrl);
-        playlist.push({
-            title: songInfo.title,
-            url: songInfo.video_url,
-            thumbnail: songInfo.player_response.videoDetails.thumbnail.thumbnails[2].url,
-        });
+exports.resolvePlaylist = async (arg) => {
+    const host = resolveHost(arg);
+    switch(host) {
+    case 'youtube': {
+        const videoUrls = await ytlist(arg, 'url').then(p => p.data.playlist);
+        const playlist = [];
+        for await (const videoUrl of videoUrls) {
+            const songInfo = await ytdl.getBasicInfo(videoUrl);
+            playlist.push({
+                title: songInfo.title,
+                url: songInfo.video_url,
+                thumbnail: songInfo.player_response.videoDetails.thumbnail.thumbnails[2].url,
+            });
+        }
+        return playlist;
     }
-    return playlist;
+    case 'spotify': {
+        // DUMMY
+    }
+    }
 };
 
-function validateYouTubeUrl(url) {
-    // eslint-disable-next-line no-useless-escape
-    const regExp = /^.*(youtu.be\/|v=)([^#\&\?]*).*/;
-    const match = url.match(regExp);
-    if (match && match[2]) {
-        return match[2];
+function resolveHost(arg) {
+    const YTRegEx = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/;
+    const SPRegEx = /^(http(s)?:\/\/)?(open.)?spotify?(\.com)?\/.+/;
+    if(YTRegEx.test(arg)) {
+        return 'youtube';
     }
-    return null;
+    if(SPRegEx.test(arg)) {
+        return 'spotify';
+    }
+    return 'search';
 }
-
-exports.validatePlaylistURL = (url) => {
-    // eslint-disable-next-line no-useless-escape
-    const regExp = /^.*(youtu.be\/|list=)([^#\&\?]*).*/;
-    const match = url.match(regExp);
-    if (match && match[2]) {
-        return match[2];
-    }
-    return null;
-};
